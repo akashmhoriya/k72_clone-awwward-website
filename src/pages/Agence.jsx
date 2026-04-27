0aacaa6d06f878a33f1735785f6241f809880fd1
akashmhoriya <akashmhoriyaofficial@gmail.com>
2026-04-27 22:12:31 +0530

    features update

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -79,17 +79,16 @@
 
 // export default Agence;
 
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
-import { useRef } from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
 
 const Agence = () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-  gsap.registerPlugin(ScrollTrigger)
-
-  const imageDivRef = useRef(null)
-  const imageRef = useRef(null)
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
 
   const imageArray = [
     "/images/image1.jpg",
@@ -103,55 +102,68 @@ const Agence = () => {
   ];
 
   useGSAP(function () {
-
     gsap.to(imageDivRef.current, {
       scrollTrigger: {
         trigger: imageDivRef.current,
         // markers: true,
-        start: 'top 28%',
-        end: 'top -70%',
+        start: "top 28%",
+        end: "top -70%",
         pin: true,
         pinSpacing: true,
         pinReparent: true,
-        pinType: 'transform',
+        pinType: "transform",
         scrub: 1, // smooth scrubbing with 1s easing
         anticipatePin: 1,
         invalidateOnRefresh: true,
         onUpdate: (elem) => {
           let imageIndex;
           if (elem.progress < 1) {
-            imageIndex = Math.floor(elem.progress * imageArray.length)
+            imageIndex = Math.floor(elem.progress * imageArray.length);
           } else {
-            imageIndex = imageArray.length - 1
+            imageIndex = imageArray.length - 1;
           }
-          imageRef.current.src = imageArray[imageIndex]
-        }
-      }
-    })
-  })
-
+          imageRef.current.src = imageArray[imageIndex];
+        },
+      },
+    });
+  });
 
   return (
-    <div className='parent'>
-      <div id='page1' className='py-1'>
-        <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-4xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-96 -top-80 lg:left-[30vw] left-[30vw]'>
-          <img ref={imageRef} className='h-full object-cover w-full' src="/images/image1.jpg" alt="" />
+    <div className="parent">
+      <div id="page1" className="py-1">
+        <div
+          ref={imageDivRef}
+          className="absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-4xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-96 -top-80 lg:left-[30vw] left-[30vw]"
+        >
+          <img
+            ref={imageRef}
+            className="h-full object-cover w-full"
+            src="/images/image1.jpg"
+            alt=""
+          />
         </div>
-        <div className='relative font-[font2]'>
-          <div className='lg:mt-[57vh] mt-[30vh]'>
-            <h1 className='text-[20vw] text-center uppercase leading-[17vw] text-black'>Soixan7e <br />
-              Douze</h1>
+        <div className="relative font-[font2]">
+          <div className="lg:mt-[57vh] mt-[30vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[17vw] text-black">
+              Soixan7e <br />
+              Douze
+            </h1>
           </div>
-          <div className='lg:pl-[41%] lg:mt-4 mt-4'>
-            <p className='lg:text-5xl text-xl selection:bg-[#D3FD50] leading-14 text-black indent-[5.5em]'>Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+          <div className="lg:pl-[41%] lg:mt-4 mt-4">
+            <p className="lg:text-5xl text-xl selection:bg-[#D3FD50] leading-14 text-black indent-[5.5em]">
+              Notre curiosité nourrit notre créativité. On reste humbles et on
+              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
+              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
+              peut faire de bons chiffres à court terme, mais on la tue à long
+              terme. C’est pour ça qu’on s’engage à donner de la perspective,
+              pour bâtir des marques influentes.
+            </p>
           </div>
         </div>
       </div>
-      <div id='page2' className=" h-screen">
-
-      </div>
+      <div id="page2" className=" h-screen"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Agence
+export default Agence;
